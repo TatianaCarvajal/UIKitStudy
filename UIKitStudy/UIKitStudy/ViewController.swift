03d606330c51456ab4dd2ea7eabbb3546cdb53a4
Tatiana Carvajal <taticv159912@gmail.com>
2023-02-09 14:50:16 -0500

    first commit

---
+++ UIKitStudy/UIKitStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitStudy
+//
+//  Created by Tatiana Carvajal on 9/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
